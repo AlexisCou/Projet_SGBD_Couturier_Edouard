f6af7e1667bb0a9f84e687c803802cbc2e2bb998
Alexis Couturier <alexis.couturier9@etu.univ-lorraine.fr>
2026-02-11 16:31:13 +0100

    modification index pour se connecter en temps que serveur

--- SGBD/index.php
+++ SGBD/index.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
@@ -6,19 +7,37 @@ error_reporting(E_ALL);
 
 require_once 'vendor/autoload.php';
 
-use SGBD\models\commande;
-use SGBD\models\plat;
-use SGBD\models\reservation;
-use SGBD\models\tabl;
-
+use SGBD\models\serveur; 
 use Illuminate\Database\Capsule\Manager as Capsule;
 
 $conf = parse_ini_file('src/conf/conf.ini');
-
 $capsule = new Capsule;
 $capsule->addConnection($conf);
 $capsule->setAsGlobal();
 $capsule->bootEloquent();
+
 $capsule->getConnection()->getPdo()->setAttribute(PDO::ATTR_AUTOCOMMIT, false);
 
-echo "<h1>Projet SGBD EDOUARD Justin COUTURIER Alexis</h1>";
+if (isset($_POST['login_submit'])) {
+    $serv = serveur::where('login', $_POST['login'])->first();
+    
+    if ($serv && $serv->mdp === $_POST['password']) {
+        $_SESSION['user'] = $serv;
+    } else {
+        $error = "Identifiants invalides";
+    }
+}
+
+if (!isset($_SESSION['user'])) {
+    echo '<h2>Connexion Serveur</h2>';
+    if(isset($error)) echo "<p style='color:red'>$error</p>";
+    echo '<form method="POST">
+            Login: <input type="text" name="login"><br>
+            Mdp: <input type="password" name="password"><br>
+            <input type="submit" name="login_submit" value="Se connecter">
+          </form>';
+    exit();
+}
+
+echo "Bonjour " . $_SESSION['user']->nom . " !";
+?>
