81960a4927aefeab988b48af11feb77410fc836d
JustinEdouard88 <justin.edouard4@etu.univ-lorraine.fr>
2026-02-11 15:38:26 +0100

    modification de test.php

--- SGBD/test.php
+++ SGBD/test.php
@@ -1,3 +1,26 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once 'vendor/autoload.php';
+
+use SGBD\models\commande;
+use SGBD\models\plat;
+use SGBD\models\reservation;
+use SGBD\models\tabl;
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+$conf = parse_ini_file('src/conf/conf.ini');
+
+$capsule = new Capsule;
+$capsule->addConnection($conf);
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
+
+$commmandes = commande::all();
+foreach ($commmandes as $commande) {
+    echo $commande->quantite . "\n";
+}
